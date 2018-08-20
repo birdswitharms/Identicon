8a7e2d3aa400fb530f69616e1588de917c9787a7
ANDREW SEGAL <segal.a@hotmail.com>
2018-08-20 10:30:40 -0400

    Build out the draw image function and save function, install :edg dep

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,6 +7,47 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input_string)
+  end
+
+  def save_image(image, input_string) do
+    File.write("#{input_string}_Identicon.png", image)
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image_struct) do
+    pixel_map = Enum.map grid, fn({_num, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+
+    %Identicon.Image{image_struct | pixel_map: pixel_map}
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image_struct) do
+    grid = Enum.filter grid, fn({num, _index}) ->
+      rem(num, 2) == 0
+    end
+
+    %Identicon.Image{image_struct | grid: grid}
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image_struct) do
